807852ea9a2c3c4a8ac6e5914bf39a5103c0e98f
rynidja <robot.rayane@gmail.com>
2024-04-22 14:11:44 +0100

    removed 'hehe'

--- app/services/google_auth_service.py
+++ app/services/google_auth_service.py
@@ -35,8 +35,6 @@ async def get_google_token(code, redirect_uri) -> GoogleToken:
 
         if response.status_code == 200:
             token_data = response.json()
-            with open("hehe.txt", "w") as f:
-                f.write(str(token_data))
 
             return GoogleToken.model_validate(token_data)
 
